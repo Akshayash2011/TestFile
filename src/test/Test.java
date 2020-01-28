a56cca63b278d2c1ea3df734d071f5d4b1e92e5f
akshaykb2011 <akshaykb2011@gmail.com>
2020-01-28 22:27:29 +0530

    bcb

--- src/test/Test.java
+++ src/test/Test.java
@@ -9,6 +9,7 @@ public class Test   {
 			 public static void main(String[] args) {
 				 try {
 						System.out.println("Starting of try block");
+					System.out.println("Mine");
 				 }	catch(Exception exp){
 						System.out.println("Catch Block") ;
 						System.out.println(exp) ;
