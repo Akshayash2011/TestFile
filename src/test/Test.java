bcb42dc44015c75049dd20a944d352d8749bb41c
akshaykb2011 <akshaykb2011@gmail.com>
2020-01-28 22:57:32 +0530

    Akshay

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,7 +8,7 @@ public class Test   {
 	
 			 public static void main(String[] args) {
 				 try {
-						System.out.println("String of try block");
+						System.out.println("Strng of try block");
 					System.out.println("Mine");
 				 }	catch(Exception exp){
 						System.out.println("Catch Block") ;
